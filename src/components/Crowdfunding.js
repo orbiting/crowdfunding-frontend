0edf5f3b855e0868593004ab39a530f77b0fc17f
patte <p@tte.io>
2017-03-05 01:57:31 +0100

    experimental: shopping cart and submitPledge migration

--- src/components/Crowdfunding.js
+++ src/components/Crowdfunding.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component, PropTypes} from 'react'
 
 import gql from 'graphql-tag'
 import {graphql} from 'react-apollo'
@@ -29,25 +29,109 @@ query {crowdfundings {
 }}
 `
 
-const Crowdfunding = ({crowdfunding}) => (
-  <div>
-    <h2>{crowdfunding.name}</h2>
-    <ul>
-      {crowdfunding.packages.map( (package_, i) => (
-        <li key={i}>
-          <h3>{package_.name}</h3>
-          {package_.options.map( (option, i) => (
-            <p>
-              <strong>{option.reward.name}</strong>&nbsp; CHF {option.price/100.0}<br/>
-              option.id: {option.id}<br/>
-              reward.id: {option.reward.id}<br/>
-            </p>
+class Crowdfunding extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      pledgeOptions: [],
+      total: 0
+    }
+  }
+
+  chooseOption(option, event) {
+    const { pledgeOptions } = this.state
+
+		// upsert pledgeOption
+    let pledgeOption = pledgeOptions.find( (po) => { return po.templateId == option.id })
+    if(pledgeOption) {
+      pledgeOption.amount += 1
+    } else {
+      pledgeOption = {
+        templateId: option.id,
+        price: option.price,
+        amount: 1
+      }
+      pledgeOptions.push(pledgeOption)
+    }
+
+		// calc total
+    let total = 0
+    pledgeOptions.forEach( (po) => { total += (po.price*po.amount) } )
+
+    this.setState( {total, pledgeOptions} )
+  }
+
+  submitPledge() {
+    const {total, pledgeOptions} = this.state
+
+    this.props.mutate({ variables: { total, packageOptions: pledgeOptions } })
+      .then(({ data }) => {
+        console.log('got data', data)
+      }).catch((error) => {
+        console.log('there was an error sending the query', error)
+      })
+  }
+
+  render() {
+    const {crowdfunding, loading } = this.props
+    const {total, pledgeOptions} = this.state
+
+    if (loading) {
+      return <span>...</span>
+    }
+
+    return (
+      <div>
+        <h2>{crowdfunding.name}</h2>
+
+        <h3>Cart</h3>
+        <strong>Total: {total}</strong>
+        {pledgeOptions.map( (option, ii) => (
+          <p key={1000+ii}>
+            {option.templateId} - {option.amount}
+          </p>
+        ))}
+        <button onClick={this.submitPledge.bind(this)}>Submit</button>
+
+        <h3>Offers</h3>
+        <ul>
+          {crowdfunding.packages.map( (package_, i) => (
+            <li key={i} >
+              <h3>{package_.name}</h3>
+              {package_.options.map( (option, ii) => (
+                <p key={i*10+ii} onClick={() => this.chooseOption(option)}>
+                  <strong>{option.reward.name}</strong> CHF {option.price/100.0}<br/>
+                  option.id: {option.id}<br/>
+                  reward.id: {option.reward.id}<br/>
+                </p>
+              ))}
+            </li>
           ))}
-        </li>
-      ))}
-    </ul>
-  </div>
-)
+        </ul>
+      </div>
+    )
+  }
+}
+
+Crowdfunding.propTypes = {
+  mutate: PropTypes.func.isRequired,
+};
+
+const submitPledge = gql`
+  mutation submitPledge($total: Int!, $packageOptions: [PackageOptionInput!]!) {
+    submitPledge(pledge: {total: $total, packageOptions: $packageOptions} ) {
+      id
+      total
+      status
+      packageId
+      packageOptions {
+        amount
+        price
+        templateId
+      }
+    }
+  }
+`
 
 const CrowdfundingWithQuery = graphql(query, {
   props: ({data}) => {
@@ -63,4 +147,7 @@ const CrowdfundingWithQuery = graphql(query, {
   }
 })(Crowdfunding)
 
-export default CrowdfundingWithQuery
+const CrowdfundingWithMutation = graphql(submitPledge)(CrowdfundingWithQuery);
+
+
+export default CrowdfundingWithMutation
