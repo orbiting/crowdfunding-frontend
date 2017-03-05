3630f6ca163bddf26f5a376cdd72cec73ec1c561
patte <p@tte.io>
2017-03-05 14:09:07 +0100

    display pledges of user

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react'
 import App from '../src/components/App'
 import Center from '../src/components/Center'
 import Crowdfunding from '../src/components/Crowdfunding'
+import Pledges from '../src/components/Pledges'
 import withData from '../src/apollo/withData'
 import {Router} from '../routes'
 
@@ -14,6 +15,7 @@ const Index = ({loading}) => {
 	return (
 		<div>
       <Crowdfunding />
+      <Pledges />
 		</div>
 	)
 }
---
+++ src/components/Pledges.js
@@ -0,0 +1,72 @@
+import React, {Component, PropTypes} from 'react'
+
+import gql from 'graphql-tag'
+import {graphql} from 'react-apollo'
+import {Link} from '../../routes'
+
+const query = gql`
+query {pledges {
+  id
+  total
+  createdAt
+  options {
+    id
+    price
+    reward {
+      ... on MembershipType {name}
+      ... on Goodie {name}
+    }
+  }
+}}
+`
+
+class Pledges extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render() {
+    const {pledges, loading } = this.props
+
+    if (loading) {
+      return <span>...</span>
+    }
+
+    return (
+      <div>
+        <h2>Your pledges</h2>
+        <ul>
+          {pledges.map( (pledge, i) => (
+            <li key={pledge.id} >
+              <h4>Pledge: {pledge.id} - CHF {pledge.total/100.0} - {pledge.createdAt}</h4>
+              {pledge.options.map( (option, ii) => (
+                <p key={option.id}>
+                  <strong>{option.reward.name}</strong> CHF {option.price/100.0}<br/>
+                  option.id: {option.id}<br/>
+                </p>
+              ))}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+const PledgesWithQuery = graphql(query, {
+  props: ({data}) => {
+    const pledges = data.pledges
+      ? data.pledges
+      : []
+
+    return {
+      loading: data.loading,
+      error: data.error,
+      pledges
+    }
+  }
+})(Pledges)
+
+export default PledgesWithQuery
