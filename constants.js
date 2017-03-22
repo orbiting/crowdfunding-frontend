f0c368dec6148544a04e61a73d789da5f9e0541a
patte <p@tte.io>
2017-03-22 21:29:28 +0100

    update default API_BASE_URL

--- constants.js
+++ constants.js
@@ -1,5 +1,6 @@
 const ENV = typeof process !== 'undefined' ? process.env : {}
 
 exports.PORT = ENV.PORT || 3000
-exports.API_BASE_URL = ENV.API_BASE_URL || 'https://api.project-r.space'
+exports.API_BASE_URL = ENV.API_BASE_URL || 'https://api.satellit.in'
+// exports.API_BASE_URL = ENV.API_BASE_URL || 'http://localhost:3001'
 exports.API_AUTHORIZATION_HEADER = ENV.API_AUTHORIZATION_HEADER
