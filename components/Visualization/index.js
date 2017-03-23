8258366cd042be03550505ebaccc4d2f8cb59a98
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-03-23 14:44:25 +0100

    add online 2010

--- components/Visualization/index.js
+++ components/Visualization/index.js
@@ -10,6 +10,8 @@ export default () => (
     <img src='/static/visualization/2001.png' style={{width: '100%'}} />
     <H2>Print 2015</H2>
     <img src='/static/visualization/2015.png' style={{width: '100%'}} />
+    <H2>Online 2010</H2>
+    <img src='/static/visualization/online-2010.png' style={{width: '100%'}} />
     <H2>Online 2015</H2>
     <img src='/static/visualization/online-2015.png' style={{width: '100%'}} />
   </div>
