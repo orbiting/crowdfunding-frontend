15838700bca44c058b64e7dfc0bab242cde2d643
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-03-23 12:48:33 +0100

    add vis example

---
+++ components/Visualization/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import {
+  H2
+} from '@project-r/styleguide'
+
+export default () => (
+  <div>
+    <H2>Print 2001</H2>
+    <img src='/static/visualization/2001.png' style={{width: '100%'}} />
+    <H2>Print 2015</H2>
+    <img src='/static/visualization/2015.png' style={{width: '100%'}} />
+  </div>
+)
