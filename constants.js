25e063f4d9beff979313bbdc33e62d8932d5613f
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-03-23 15:28:21 +0100

    pass apollo config from server to client

--- constants.js
+++ constants.js
@@ -2,5 +2,4 @@ const ENV = typeof process !== 'undefined' ? process.env : {}
 
 exports.PORT = ENV.PORT || 3000
 exports.API_BASE_URL = ENV.API_BASE_URL || 'https://api.satellit.in'
-// exports.API_BASE_URL = ENV.API_BASE_URL || 'http://localhost:3001'
 exports.API_AUTHORIZATION_HEADER = ENV.API_AUTHORIZATION_HEADER
