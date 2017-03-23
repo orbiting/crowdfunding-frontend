b5a3dbeb05654f5aa8a6c5dc9a74c95ef1f0b7d7
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-03-23 12:59:04 +0100

    add online example

--- components/Visualization/index.js
+++ components/Visualization/index.js
@@ -10,5 +10,7 @@ export default () => (
     <img src='/static/visualization/2001.png' style={{width: '100%'}} />
     <H2>Print 2015</H2>
     <img src='/static/visualization/2015.png' style={{width: '100%'}} />
+    <H2>Online 2015</H2>
+    <img src='/static/visualization/online-2015.png' style={{width: '100%'}} />
   </div>
 )
