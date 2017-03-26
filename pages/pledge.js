28a2c654528da21999d37433a3b31dab44eb8c0a
patte <p@tte.io>
2017-03-26 21:09:48 +0200

    checkout: check if email is taken: wait for login if so

--- pages/pledge.js
+++ pages/pledge.js
@@ -3,6 +3,9 @@ import withData from '../lib/withData'
 import App from '../components/App'
 import Accordion from '../components/Accordion'
 import Router from 'next/router'
+import { gql, withApollo } from 'react-apollo'
+import withSession from '../lib/auth/with-session'
+import Session from '../lib/auth/session'
 
 import {
   Button,
@@ -10,10 +13,18 @@ import {
   NarrowContainer
 } from '@project-r/styleguide'
 
+let CHECK_EMAIL_TIMEOUT = null
+
 class Pledge extends Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    const session = this.props.session || {}
+    const isLoggedIn = (session.user ? true : false)
+    this.state = {
+      emailFree: true,
+      isLoggingIn: false,
+      isLoggedIn
+    }
 
     this.amountRefSetter = (ref) => {
       this.amountRef = ref
@@ -24,9 +35,24 @@ class Pledge extends Component {
       this.amountRef.input.focus()
     }
   }
+  componentWillReceiveProps (nextProps) {
+    const {session } = this.props
+    //set email & name if logged in
+    if(session && session.user && !email) {
+      //TODO name
+      this.setState( {email: session.user.email} )
+    }
+  }
   render () {
-    const {name, email} = this.state
-    const {query} = this.props
+    const {
+      name,
+      email,
+      emailFree,
+      isLoggingIn,
+      isLoggedIn
+    } = this.state
+    const {query, client, session } = this.props
+
 
     const handleChange = field => {
       return event => {
@@ -36,6 +62,51 @@ class Pledge extends Component {
         }))
       }
     }
+    const handleEmailChange = field => {
+      return event => {
+        const value = event.target.value
+        this.setState( {email: value} )
+        //check if email is untaken or we need to login
+        //throttle
+        if(CHECK_EMAIL_TIMEOUT) { clearTimeout(CHECK_EMAIL_TIMEOUT) }
+        CHECK_EMAIL_TIMEOUT = setTimeout( async () => {
+          const {loading, data} = await client.query({
+            query: gql`
+              query checkEmail($email: String!) {
+                checkEmail(email: $email) {
+                  free
+                }
+              }
+            `,
+            variables: { email: value }
+          })
+          console.log("free: "+data.checkEmail.free)
+          this.setState(
+            { emailFree: data.checkEmail.free }
+          )
+        }, 300)
+
+      }
+    }
+
+    const continueWithLogin = field => {
+      return async (event) => {
+        this.setState( {isLoggingIn: true } )
+        const session = new Session()
+        await session.signin(this.state.email)
+        try {
+          const authenticatedSession = await session.authenticatedSession()
+          this.setState({
+            isLoggingIn: false,
+            isLoggedIn: true
+          })
+        } catch (e) {
+          console.log("timeout")
+          this.setState( {isLoggingIn: false } )
+        }
+      }
+    }
+
 
     return (
       <div>
@@ -83,13 +154,18 @@ class Pledge extends Component {
 
         <H2>Deine Kontaktinformationen</H2>
         <p style={{marginTop: 0}}>
+          {isLoggedIn && (
+            <strong>Du bist eingeloggt als:</strong>
+          )}
           <Field label='Dein Name'
             value={name}
+            disabled={isLoggedIn}
             onChange={handleChange('name')} />
           <br />
           <Field label='Deine E-Mail'
             value={email}
-            onChange={handleChange('email')} />
+            disabled={isLoggedIn}
+            onChange={handleEmailChange()} />
         </p>
 
         <H2>Zahlungsart auswählen</H2>
@@ -123,22 +199,44 @@ class Pledge extends Component {
             {' '}Twint
           </label><br />
         </P>
+        {(!emailFree && !isLoggedIn && !isLoggingIn) && (
+          <div key='needsLogin'>
+            <p>Es existiert bereits ein Account mit dieser Email adresse bei uns. Um weiter zu fahren, müssen Sie sich erst einloggen. Klicken Sie auf Einloggen oder wählen sie eine andere email adresse.</p>
+            <Button onClick={continueWithLogin()}>Einloggen</Button>
+          </div>
+        )}
+        {(!emailFree && !isLoggedIn && isLoggingIn) && (
+          <div key='waitingForLogin'>
+            Wir haben Ihnen eine email geschickt. Bitte klicken Sie den Link darin an um einzuloggen.
+          </div>
+        )}
 
-        <Button disabled>Weiter</Button>
+        {(emailFree || isLoggedIn) && (
+          <span key='payment'>
+            <H2>Zahlungsart auswählen</H2>
+
+            <Button onClick={submitPledge}>Weiter</Button>
+          </span>
+        )}
+
       </div>
     )
   }
 }
 
 Pledge.propTypes = {
-  query: PropTypes.object.isRequired
+  query: PropTypes.object.isRequired,
+  client: PropTypes.object.isRequired,
+  session: PropTypes.object.isRequired,
 }
 
-export default withData(({url}) => (
+const PledgeWithApollo = withApollo(Pledge)
+
+export default withSession(withData( ({url, session}) => (
   <App>
     <NarrowContainer>
       <H1>Mitmachen</H1>
-      <Pledge query={url.query} />
+      <PledgeWithApollo query={url.query} session={session} />
     </NarrowContainer>
   </App>
-))
+)))
