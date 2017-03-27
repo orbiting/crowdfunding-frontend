4b547754cd137b2ff6068faef6d5c2d8e872ce47
Lukas Bünger <lukasbuenger@gmail.com>
2017-03-27 12:12:40 +0200

    display database faq items in list

--- pages/faq.js
+++ pages/faq.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import { gql, withApollo, graphql } from 'react-apollo'
+
 import withData from '../lib/withData'
 import App from '../components/App'
 
 import {
   H1, P, MediumContainer
 } from '@project-r/styleguide'
+
+const publishedFaqs = gql`
+query {
+  faqs(status: PUBLISHED) {
+    id
+    status
+    question
+    answer
+    createdAt
+    updatedAt
+  }
+}
+`
+
+const FaqList = ({data: {faqs}}) => (
+  <div>{
+    faqs.map(entry => (
+      <P key={entry.id}>{ `${entry.question}: ${entry.answer}` }</P>
+    ))
+  }</div>
+)
+
+const ConnectedFaqList = graphql(publishedFaqs)(withApollo(FaqList))
 
 export default withData((props) => (
   <App>
@@ -13,6 +38,7 @@ export default withData((props) => (
       <P>
         Antworten zu den brennenden Fragen.
       </P>
+      <ConnectedFaqList />
     </MediumContainer>
   </App>
 ))
