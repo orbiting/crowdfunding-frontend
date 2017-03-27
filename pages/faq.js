0e6400c088ac4e239c038e49477db8221dd1ec46
Lukas Bünger <lukasbuenger@gmail.com>
2017-03-27 14:46:53 +0200

    Question form reacts to mutation result

--- pages/faq.js
+++ pages/faq.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import { gql, withApollo, graphql } from 'react-apollo'
+import React, { Component } from 'react'
+import { gql, graphql } from 'react-apollo'
 
 import withData from '../lib/withData'
 import App from '../components/App'
 
 import {
-  H1, P, MediumContainer
+  H1, P, MediumContainer, Field, Button
 } from '@project-r/styleguide'
 
+/*
+List of published questions and answers
+ */
 const publishedFaqs = gql`
 query {
   faqs(status: PUBLISHED) {
@@ -29,8 +32,69 @@ const FaqList = ({data: {faqs}}) => (
   }</div>
 )
 
-const ConnectedFaqList = graphql(publishedFaqs)(withApollo(FaqList))
+const ConnectedFaqList = graphql(publishedFaqs)(FaqList)
 
+/*
+Question form
+ */
+const submitQuestion = gql`
+mutation submitQuestion($question: String!) {
+  submitQuestion(question: $question) {
+    success
+  }
+}
+`
+
+class QuestionForm extends Component {
+  constructor (...args) {
+    super(...args)
+    this.state = {
+      successfullySubmitted: false
+    }
+
+    this.questionRefSetter = (ref) => {
+      this.questionRef = ref
+    }
+    this.submitHandler = this.submitHandler.bind(this)
+  }
+
+  submitHandler () {
+    const question = this.questionRef.input.value
+    this.props
+      .mutate({ variables: { question } })
+      .then(({ data: { success } }) => {
+        this.setState({
+          successfullySubmitted: true
+        })
+      })
+      .catch(error => {
+        // window.alert('Pledge error')
+        console.log(error)
+      })
+  }
+
+  render () {
+    return !this.state.successfullySubmitted
+      ? (
+        <div>
+          <P>Hast du Frage?</P>
+          <Field ref={this.questionRefSetter} label='Frage' />
+          <Button onClick={this.submitHandler}>Abschicken</Button>
+        </div>
+      )
+      : (
+        <div>
+          <P>This happened...</P>
+        </div>
+      )
+  }
+}
+
+const ConnectedQuestionForm = graphql(submitQuestion)(QuestionForm)
+
+/*
+Faq page
+ */
 export default withData((props) => (
   <App>
     <MediumContainer>
@@ -39,6 +103,7 @@ export default withData((props) => (
         Antworten zu den brennenden Fragen.
       </P>
       <ConnectedFaqList />
+      <ConnectedQuestionForm />
     </MediumContainer>
   </App>
 ))
