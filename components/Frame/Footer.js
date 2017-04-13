7c17d9914a92c3c011837d3e36abeaf7b5db5943
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-04-13 11:52:25 +0200

    add footer content

--- components/Frame/Footer.js
+++ components/Frame/Footer.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import {css} from 'glamor'
+import withT from '../../lib/withT'
+import {intersperse} from '../../lib/utils/helpers'
+import Link from 'next/link'
 
 import {
-  Container, Logo, colors
+  Container, Logo, colors, mediaQueries,
+  fontFamilies
 } from '@project-r/styleguide'
 
 import IconLink from '../IconLink'
+
+const COL_PADDING_S = 15
+const COL_PADDING_M = 70
 
 const styles = {
   bg: css({
@@ -19,12 +26,80 @@ const styles = {
   }),
   icons: css({
     float: 'right'
+  }),
+  grid: css({
+    marginLeft: -COL_PADDING_S,
+    marginRight: -COL_PADDING_S,
+    [mediaQueries.mUp]: {
+      marginLeft: -COL_PADDING_M,
+      marginRight: -COL_PADDING_M
+    },
+    ':after': {
+      content: '""',
+      display: 'table',
+      clear: 'both'
+    }
+  }),
+  column: css({
+    paddingLeft: COL_PADDING_S,
+    paddingRight: COL_PADDING_S,
+    fontSize: 12,
+    lineHeight: '18px',
+    fontFamily: fontFamilies.sansSerifRegular,
+    color: colors.secondary,
+    float: 'left',
+    width: '50%',
+    [mediaQueries.mUp]: {
+      fontSize: 17,
+      lineHeight: '25px',
+      paddingLeft: COL_PADDING_M,
+      paddingRight: COL_PADDING_M,
+      width: '25%'
+    },
+    '& a': {
+      textDecoration: 'none',
+      color: colors.secondary,
+      ':visited': {
+        color: colors.secondary
+      },
+      ':hover': {
+        color: colors.primary
+      }
+    }
+  }),
+  title: css({
+    fontFamily: fontFamilies.sansSerifMedium
+  }),
+  hr: css({
+    marginTop: 20,
+    marginBottom: 20,
+    border: 'none',
+    borderBottom: '1px solid #DADDDC'
   })
 }
 
-const Footer = () => (
+const Footer = ({t}) => (
   <div {...styles.bg}>
     <Container>
+      <div {...styles.grid}>
+        <div {...styles.column}>
+          <div {...styles.title}>{t('footer/contact/title')}</div>
+          <a href='https://www.google.ch/maps/place/Sihlhallenstrasse+1,+8004+Zürich' target='_blank'>{intersperse(
+            t('footer/contact/address').split('\n'),
+            (item, i) => <br key={i} />
+          )}</a>
+        </div>
+        <div {...styles.column}>
+          <div {...styles.title}>{t('footer/about/title')}</div>
+          <a href='https://project-r.construction/' target='_blank'>{t('footer/about/projecR')}</a>
+
+        </div>
+        <div {...styles.column}>
+          <div {...styles.title}>{t('footer/legal/title')}</div>
+          <Link href='/impressum'><a>{t('footer/legal/imprint')}</a></Link>
+        </div>
+      </div>
+      <hr {...styles.hr} />
       <Logo fill={colors.secondary} width={140} />
       <div {...styles.icons}>
         <IconLink icon='facebook' href='https://www.facebook.com/RepublikMagazin' target='_blank' fill={colors.secondary} />
@@ -34,4 +109,4 @@ const Footer = () => (
   </div>
 )
 
-export default Footer
+export default withT(Footer)
