3b4996f41c8c2ea3b0d4e817701d05a079f76eb2
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-04-14 12:41:30 +0200

    setup basic page

--- pages/merci.js
+++ pages/merci.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {compose} from 'redux'
+import {gql, graphql} from 'react-apollo'
+import Router from 'next/router'
 
 import withData from '../lib/withData'
 import Frame from '../components/Frame'
@@ -7,58 +9,173 @@ import withMe from '../lib/withMe'
 import withT from '../lib/withT'
 import Poller from '../components/Auth/Poller'
 import SignIn from '../components/Auth/SignIn'
-import SignOut from '../components/Auth/SignOut'
+import {withSignOut} from '../components/Auth/SignOut'
+import Loader from '../components/Loader'
+import Share from '../components/Share'
+import {timeFormat} from '../lib/utils/formats'
 
 import {
-  H1, P,
+  PUBLIC_BASE_URL
+} from '../constants'
+
+import {
+  H1, P, Button, Lead,
+  H2, Label, A,
   NarrowContainer
 } from '@project-r/styleguide'
 
+const dateTimeFormat = timeFormat('%d. %B %Y %H:%M')
+
+const pledgesQuery = gql`
+query pledges {
+  me {
+    id
+    pledges {
+      id
+      package {
+        name
+      }
+      options {
+        reward {
+          ... on MembershipType {
+            name
+          }
+          ... on Goodie {
+            name
+          }
+        }
+        minAmount
+        maxAmount
+        amount
+      }
+      status
+      total
+      payments {
+        method
+        paperInvoice
+        total
+        status
+        hrid
+        createdAt
+        updatedAt
+      }
+      memberships {
+        voucherCode
+      }
+      createdAt
+    }
+  }
+}
+`
+
 const Merci = compose(
+  graphql(pledgesQuery, {
+    props: ({data}) => {
+      return {
+        loading: data.loading,
+        error: data.error,
+        pledges: data.me ? data.me.pledges : []
+      }
+    }
+  }),
+  withSignOut,
   withMe,
   withT
-)(({me, t, url: {query}}) => {
-  if (query.email && query.id) {
+)((props) => {
+  const {me, t, url: {query}} = props
+  if (!me) {
+    if (query.email && query.phrase) {
+      return (
+        <P>
+          {t('merci/postpay/waiting', {
+            email: query.email,
+            phrase: query.phrase
+          })}<br />
+          <Poller onSuccess={() => {}} />
+        </P>
+      )
+    }
     return (
       <div>
-        {
-          me ? (
-            <H1>
-              {t('merci/postpay/title/me', {
-                name: me.name
-              })}
-            </H1>
-          ) : (
-            <P>
-              {t('merci/postpay/waiting', {
-                email: query.email,
-                phrase: query.phrase
-              })}<br />
-              <Poller onSuccess={() => {}} />
-            </P>
-          )
-        }
+        <h1>{t('merci/signedOut/title')}</h1>
+        <P>
+          {t('merci/signedOut/signIn')}
+        </P>
+        <SignIn />
       </div>
     )
   }
+
+  const {loading, error, pledges} = props
   return (
-    <div>
-      <H1>{t('merci/title')}</H1>
-      {me ? (
+    <Loader loading={loading} error={error} render={() => {
+      if (!pledges.length) {
+        return (
+          <div>
+            <h1>{t('merci/empty/title', {
+              nameOrEmail: me.name || me.email
+            })}</h1>
+            <P>
+              {t('merci/empty/text')}
+            </P>
+            <Button primary onClick={() => {
+              Router.push('/pledge')
+                .then(() => window.scrollTo(0, 0))
+            }}>
+              {t('merci/empty/button')}
+            </Button>
+          </div>
+        )
+      }
+
+      return (
         <div>
-          <P>{me.name} {me.email}</P>
-          <SignOut />
-          <P>TK</P>
-        </div>
-      ) : (
-        <div>
+          <H1>{t('merci/title', {
+            name: me.name
+          })}</H1>
+          <Lead>
+            {t('merci/lead')}
+          </Lead>
           <P>
-            {t('merci/signin')}
+            <Share
+              url={`${PUBLIC_BASE_URL}/`}
+              tweet={t('merci/share/tweetTemplate')}
+              emailSubject={t('merci/share/emailSubject')}
+              emailBody={t('merci/share/emailBody', {
+                url: `${PUBLIC_BASE_URL}/`,
+                backerName: me.name
+              })}
+              emailAttachUrl={false} />
           </P>
-          <SignIn />
+          {pledges.map(pledge => {
+            const configurableOptions = pledge.options.filter(option => (
+              option.minAmount !== option.maxAmount
+            ))
+            const createdAt = new Date(pledge.createdAt)
+            return (
+              <div key={pledge.id} style={{marginBottom: 30}}>
+                <H2 style={{marginBottom: 0}}>{t(`package/${pledge.package.name}/title`)}</H2>
+                <Label>{dateTimeFormat(createdAt)}</Label>
+                {!!configurableOptions.length && (
+                  <ul>
+                    {configurableOptions.map((option, i) => (
+                      <li key={i}>{option.amount} x {option.name}</li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            )
+          })}
+          <A href='#' onClick={(e) => {
+            e.preventDefault()
+            this.props.signOut().then(() => {
+              this.handleName('', false, t)
+              this.handleEmail('', false, t)
+            })
+          }}>{t('merci/signOut')}</A>
         </div>
-      )}
-    </div>
+      )
+    }} />
   )
 })
 
