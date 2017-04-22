7c758d6b19b95afe73ee99ba8deadfe1d0b2036b
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-04-22 21:07:33 +0200

    share layout

--- components/Testimonial/Share.js
+++ components/Testimonial/Share.js
@@ -8,24 +8,75 @@ import withT from '../../lib/withT'
 import Loader from '../Loader'
 
 import {
-  Interaction, P
+  P, Logo, fontFamilies
 } from '@project-r/styleguide'
-
-const {H2} = Interaction
 
 const styles = {
   container: css({
+    position: 'relative',
     width: 1200,
     height: 628
+  }),
+  logo: css({
+    position: 'absolute',
+    left: 628 + 50,
+    right: 50,
+    bottom: 50
+  }),
+  image: css({
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    width: 628
+  }),
+  text: css({
+    position: 'absolute',
+    top: 50,
+    left: 628 + 50,
+    right: 50,
+    bottom: 50 + 120,
+    wordWrap: 'break-word'
+  }),
+  quote: css({
+    fontSize: 27,
+    lineHeight: 1.42
+  }),
+  number: css({
+    fontSize: 30,
+    fontFamily: fontFamilies.sansSerifMedium
   })
 }
 
-const Item = ({loading, error, testimonial: {name, role, quote, image, video}}) => (
+const fontSizeBoost = length => {
+  if (length < 40) {
+    return 26
+  }
+  if (length < 50) {
+    return 17
+  }
+  if (length < 80) {
+    return 8
+  }
+  if (length < 100) {
+    return 4
+  }
+}
+
+const Item = ({loading, error, testimonial: {quote, image, video}}) => (
   <Loader loading={loading} error={error} render={() => (
     <div {...styles.container}>
-      <img src={image} />
-      <H2>{name} <span>{role}</span></H2>
-      <P>«{quote}»</P>
+      <img {...styles.image} src={image} />
+      <div {...styles.text}>
+        <P {...styles.quote}
+          style={{fontSize: 24 + fontSizeBoost(quote.length)}}>
+          «{quote}»
+        </P>
+        <div {...styles.number}>Abo #59</div>
+      </div>
+      <div {...styles.logo}>
+        <Logo />
+      </div>
     </div>
   )} />
 )
@@ -37,6 +88,7 @@ const query = gql`query testimonials($firstId: ID) {
     role
     quote
     image
+    sequenceNumber
     video {
       hls
       mp4
