7174e0839d4280f99ee79cfe0e93520053127f0e
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-04-22 21:46:41 +0200

    accept booked Postcard payments

--- components/Pledge/ReceivePayment.js
+++ components/Pledge/ReceivePayment.js
@@ -43,7 +43,7 @@ class PledgeReceivePayment extends Component {
 
     const state = this.state = {}
     if (query.orderID) {
-      if (query.STATUS === '5') {
+      if (query.STATUS === '5' || query.STATUS === '9') {
         state.processing = true
         state.action = {
           method: 'pay',
