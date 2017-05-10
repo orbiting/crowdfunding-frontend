90f18f52f2c45f3f8e9e268b21d153ae473413f4
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-05-10 02:02:55 +0200

    give text bed

--- components/Stats/Memberships.js
+++ components/Stats/Memberships.js
@@ -4,8 +4,10 @@ import {compose} from 'redux'
 import {range} from 'd3-array'
 import {nest} from 'd3-collection'
 import {css} from 'glamor'
+import md from 'markdown-in-js'
 
 import withT from '../../lib/withT'
+import mdComponents from '../../lib/utils/mdComponents'
 
 import Loader from '../Loader'
 
@@ -13,14 +15,15 @@ import BarChart from './BarChart'
 import PostalCodeMap from './Map'
 
 import {
-  Interaction, A, Label, colors
+  Interaction, A, Label, colors,
+  H1, H2, P
 } from '@project-r/styleguide'
 
-import {swissTime} from '../../lib/utils/formats'
+import {swissTime, countFormat} from '../../lib/utils/formats'
 
 const dateFormat = swissTime.format('%A %-d.%-m.')
 
-const {H1, H2, H3, P} = Interaction
+const {H3} = Interaction
 
 const styles = {
   dateContainer: css({
@@ -90,19 +93,36 @@ const Memberships = ({loading, error, data}) => (
     return (
       <div>
         <H1>Wer sind Sie?</H1>
-        <P>{status.people} gelöste Abos gehören zur Zeit {countries.reduce((sum, d) => sum + d.count, 0)} Personen</P>
-        <br /><br />
-        <H2>Wo leben Sie?</H2>
-        {countries.map(({name, count, postalCodes}) => (
-          <div key={name}>
-            <H3>{name || '(noch) Kein Angabe'} {count}</H3>
-            {(name === 'Schweiz' || name === 'Deutschland' || name === 'Österreich') && (
-              <div>
-                <PostalCodeMap data={postalCodes} />
-              </div>
-            )}
-          </div>
-        ))}
+        <P>Ladies and Gentlemen,</P>
+
+        <P>
+          wir haben zum Start der Republik einiges darüber geschrieben, wer wir sind. Nun ist ein Drittel der Kampagne Geschichte. Und wir können endlich über ein noch interessanteres Thema reden: Wer Sie sind.
+        </P>
+
+        <P>
+          Denn darüber besteht einige Unklarheit. Immerhin haben Sie den Weltrekord für Medien-Crowdfundings gebrochen. Seitdem florierten die Spekulationen: Bis zur Vermutung, dass Sie in Wahrheit eine Roboterarmee seien, die von einem geschickten Financier zwecks verdeckter Zahlung aufgeschaltet wurde.
+        </P>
+
+        <P>
+          Nun, wir wollen die Fakten sprechen lassen - selbst, wenn sie weniger interessant sind als als die Roboterarmee. Unser IT-Team hat ihre Daten (selbstverständlich anonym) ausgewertet. Und Thomas Preusse und Patrick Recher haben sie (oder sollte man schrieben: Sie?) in Grafiken verpackt.
+        </P>
+
+        <P>
+          Hier also das vorläufige Zwischenergebnis, erhoben beim Stand von {countFormat(status.people)} Mitgliedern. Sie erhalten nun die Antworten zu den Fragen, wo Sie wohnen, wie alt Sie sind, wie schnell Sie an Bord kamen und wie seriös Ihre Zahlungsmoral war.
+        </P>
+
+        { /* <P>{status.people} gelöste Abos gehören zur Zeit {countries.reduce((sum, d) => sum + d.count, 0)} Personen</P> */ }
+
+        <H2>Wo wohnen Sie?</H2>
+
+        <P>
+          Auf der unterstehenden Karte sehen Sie die Verteilung der Republik-Mitglieder: In Zürich. Dann, sobald Sie zoomen, in der Schweiz. Und schliesslich im deutschsprachigen Raum.
+        </P>
+
+        <P>
+          Jeder Punkt auf der Karte repräsentiert eine Postleitzahl. Je fetter der Punkt, desto mehr von Ihnen leben dort. (Die Summe entspricht nicht ganz {countFormat(status.people)} - da nur knapp {Math.ceil(countries.filter(d => d.name).reduce((sum, d) => sum + d.count, 0) / status.people * 100)} Prozent ihre Postadresse angaben.)
+        </P>
+
         <PostalCodeMap
           data={countries
             .filter(({name}) => name === 'Schweiz' || name === 'Deutschland' || name === 'Österreich')
@@ -110,27 +130,57 @@ const Memberships = ({loading, error, data}) => (
               (all, country) => all.concat(country.postalCodes),
               []
             )} />
+
+        <P>Ein paar Fakten dazu.</P>
+
+        <P>Zürich ist zwar eine Hochburg für die Republik. Aber bei weitem nicht das alleinige Verbreitungsgebiet. 3216 von Ihnen wohnen dort - also fast exakt ein Drittel.</P>
+        <P>In Zürich ist der Kreis 4 am dichtesten mit Republik-Abonnements gepflastert. Statistisch gesehen müssen wir auf unserem Arbeitsweg zum Hotel Rothaus jeden hundertsten mit „Guten Morgen, Verleger!“ oder „Guten Morgen, Verlegerin!“ begrüssen.</P>
+        <P>Auf Zürich folgen die Konkurrenzstädte Bern (965 Abonnentinnen), Basel (438), Winterthur (206) und - leicht abgeschlagen - Luzern (85).</P>
+        <P>Was uns besonders freut, ist die relative Stärke der Republik am Geburtsort der Helvetischen Republik, in Aarau, mit 87 Abonnenten, verstärkt durch Baden (69) und die Agglomeration von Baden (44).</P>
+        <P>Das restliche Drittel von Ihnen verstreut sich über die ganze Schweiz.</P>
+
+        {countries.map(({name, count, postalCodes}) => (
+          <div key={name}>
+            <H3>{name || '(noch) Kein Angabe'} {count}</H3>
+          </div>
+        ))}
+
+        <P>Im Ausland führt Deutschland mit 102 Republik-Mitgliedern, vor Lichtenstein mit 17, Österreich mit 12, der USA mit 11, Belgien und Italien mit 4, Spanien und Norwegen mit 3, Austalien, Dänemark, Griechenland, Hong Kong, Japan, Holland, Thailand, Grossbritannien und China mit 2 Abonnements.</P>
+
+        <P>Einen einziges Mitglied der Republik finden wir in Georgien, Uruguay, Ägypten, Kanada, Kolumbien, Korea, Mexiko, Puerto Rico, Singapur, Taiwan, Myanmar und Malawi. Ein Gruss Ihnen allen in Ihre Exklusivität und Einsamkeit!</P>
+
         <br /><br />
         <H2>Wie alt sind Sie?</H2>
         <P>
-          {ages.find(d => d.age === null).count} (noch) kein Angabe<br />
+          Bei dieser Frage machten {countFormat(ages.find(d => d.age === null).count)} Personen (noch) keine Angabe. Von den restlichen {countFormat(ages.filter(d => d.age !== null).reduce((sum, d) => sum + d.count, 0))} sind:
+        </P>
+
+        <P>
           {ages.reduce(
             (sum, d) => sum + (
               d.age !== null && d.age < 16 ? d.count : 0
             ),
             0
-          )} sind jünger als 16. Dies sind vorallem Firmen-Abos.
-          <br /><br />
+          )}
+          {' '}Abonnenten jünger als 16 Jahre. Bei den der Mehrheit handelt es sich allerdings nicht um frühreife Kinder, sondern um Firmenabonnements. Diese gaben ihr Gründungsjahr an.
+        </P>
+
+        <P>
           {ages.reduce(
             (sum, d) => sum + (
               d.age > 99 ? d.count : 0
             ),
             0
-          )} sind 100 oder älter, dies sind Eingabefehler und Falschangaben.
-          <br /><br />Zum Beispiel 1848, u.A. das Grundungsjahr des Schweizerischen Bundesstaat, 8. Dezember 1873 — der <A href='https://de.wikipedia.org/wiki/Anton_Afritsch_(Journalist)'>Geburtstag des Journalist Anton Afritsch</A> und 19. Dezember 1878 <A href='https://de.wikipedia.org/wiki/Bayard_Taylor'>Geburtstag des Reiseschriftsteller Bayard Taylor</A>.
-          <br /><br />
-          Einzig ein unverifizierter, 102 jähriger Rolf könnte unser ältester Leser sein. Wir vermuten aber eher das es eine verifizierte 92 jahrige Person ist.
+          )}
+          {' '}Abonnentinnen älter als 99 Jahre. Wir vermuten allerdings bei den meisten Eingabefehler. Oder einen symbolischen Wink. Etwa beim Geburtsjahr 1848 - dem Gründungsjahr des Schweizerischen Bundesstaates. Oder beim 8. Dezember 1873, dem Geburtstag des <A href='https://de.wikipedia.org/wiki/Anton_Afritsch_(Journalist)'>Journalisten Anton Afritsch</A> – oder beim 19. Dezember 1878, an dem der <A href='https://de.wikipedia.org/wiki/Bayard_Taylor'>Reiseschriftsteller Bayard Taylor</A> geboren wurde.
         </P>
+
+        <P>Am stärksten gefragt ist die Republik in der Altersgruppe um 33 Jahre. Das Durchschnittsalter unserer Leser und Leserinnen beträgt 44. Exakt bei diesem Alter folgt in der Statistik übrigens - für ein Internet-Medium nicht unerwartet - ein deutlicher Knick.
+        </P>
+
+        <P>Im Ganzen ist die Leserschaft der Republik einen Hauch reifer als die Bevölkerung von Zürich - aber einiges jünger als die Gesamtbevölkerung der Schweiz.
+        </P>
+
         <H3 style={{marginBottom: 20}}>
           Republik von 16 bis 92 jährige Mitglieder
         </H3>
@@ -151,7 +201,11 @@ const Memberships = ({loading, error, data}) => (
           </A>
         </div>
 
-        <H2 style={{marginTop: 40}}>Wann kaufen Sie?</H2>
+        <H2 style={{marginTop: 40}}>Wie schnell waren Sie?</H2>
+
+        <P>
+          Laut Theorie verlaufen Crowdfundings gern dramatisch: Am Anfang gibt es einen Höhepunkt, am Ende gibt es einen Höhepunkt, dazwischen dümpelt es vor sich hin. Die Republik machte dabei bisher keine Ausnahme. Hier die Abonnementskurve vom 26. April bis 1. Mai:
+        </P>
         <div {...styles.dateContainer}>
           {groupedCreatedAts.map(({key, values}) => (
             <div {...styles.dateBox}>
@@ -171,6 +225,65 @@ const Memberships = ({loading, error, data}) => (
             </div>
           ))}
         </div>
+        <br style={{clear: 'left'}} />
+
+        <P>
+          Hier noch ein paar bemerkenswerte Momente:
+          Das offizielle Ziel von 3000 Mitgliedern und 750000 Franken erreichte die Republik: Nach 7 Stunden 49 Minuten.
+          Der Schweizer Rekord für Crowdfundings fiel nach: xxxxxxxx
+          Den Weltrekord für Medien-Crowdfundings erreichten wir um: xxxxxxxx
+          Mitglied Nummer 10’000 registrierte sich etwa um:
+        </P>
+
+        <H2>
+          Wie zuverlässig zahlten Sie?
+        </H2>
+
+        <P>
+          Der irische Dichter, Trinker (und IRA-Anhänger) Brendan Behan sagte einmal: „Möge die gebende Hand nie zittern!“
+          Behan wäre stolz auf Sie gewesen. Von 2,8 Millionen Franken sind bis jetzt, noch mitten im Crowdfunding, nur 200’000 Franken ausstehend. Das ist, laut Experten, ein verblüffend kleiner Betrag.
+          Danke für Ihre Schnelligkeit – wir werden versuchen, uns daran ab Januar 2018 beim Einhalten von Redaktionsschlüssen zu erinnern!
+        </P>
+
+        <H2>
+          Wie zahlten Sie?
+        </H2>
+        <P>
+          Diese Statistik veröffentlichen wir, weil das sonst kaum eine andere Firma macht - Zahlungsdaten gelten als Geschäftsgeheimnis. Wir folgen dieser Praxis nicht - und hoffen, dass irgendwer irgendetwas aus unserer Statistik lernt.
+        </P>
+
+        <P>
+          Was uns übrigens verblüffte: Paypal schlug als Zahlungsmethose die Postfinance. (Vielleicht ein Hinweis, dass Postfinance ihre verblüffend unschöne und kundenunfreundliche Zahlungs-Website überarbeiten sollte.)
+        </P>
+
+        <H2>Wieviel von Ihnen sind auf der Community-Seite dabei?</H2>
+        <P>
+          Enorm viel. Fast ein Drittel von Ihnen - über 3000 Leute - schalteten ein Foto und einen Slogan auf Ihrer persönlichen Seite hoch.
+          Danke dafür! Für die über 3000 Fotos, die Unterstützung, die Begründungen und die Ratschläge! Stellvertretend für alle wollen wir nur eine Stimme zitieren – das vermutlich geographisch (und mental) am weitesten entfernte Mitglied, direkt aus seinem Wohnort auf dem Todesstern:
+        </P>
+
+        <P>(( Bild Stormtrooper ))</P>
+
+        <P>
+          Das war alles, was wir über Sie wissen. Ausser, natürlich, noch zwei Dinge:
+        </P>
+
+        {md(mdComponents)`
+Das war alles, was wir über Sie wissen. Ausser, natürlich, noch zwei Dinge:
+
+1. Dass Sie (Unzutreffendes streichen) entweder reich an Mut, an Vertrauen oder an Verrücktheit sind. Weil Sie Verlegerin und zukünftiger Leser eines Magazins geworden sind, von dem noch nichts existiert als ein ehrgeiziger Plan.
+2. Dass wir Ihnen für Ihren Mut, Ihr Vertrauen, Ihre Verrücktheit (Unzutreffendes streichen) verpflichtet sind - jedem und jeder einzelnen von Ihnen. Wir werden hart daran arbeiten, ein Internet-Magazin zu bauen, das Sie (nicht bei jedem Artikel, aber in der Bilanz) stolz macht, das Risiko eingegangen zu sein.
+
+Mit Dank für Ihre Kühnheit und unsere Verantwortung,
+
+Ihre Crew von der Republik und von Project R
+
+((( Namen )))
+
+PS: Falls Ihnen noch jemand einfällt, der Ihre Vorliebe für Mut, Vertrauen oder Verrücktheit teilt (Unzutreffendes - Sie wissen schon!), weisen Sie die Person auf folgende Website hin: [www.republik.ch](https://www.republik.ch/)
+
+Denn eine Republik wird nie von wenigen gegründet, sondern von vielen.
+  `}
       </div>
     )
   }} />
