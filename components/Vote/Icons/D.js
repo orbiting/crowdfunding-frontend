8b96c363fe1788227b9865a13992269f941f0a1a
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-05-22 01:40:37 +0200

    clean up

--- components/Vote/Icons/D.js
+++
@@ -1,9 +0,0 @@
-import abc from './abc'
-
-export default () => (
-  <svg width='67' height='67' viewBox='0 0 67 67'>
-    <g transform='translate(-3 -6)'>
-      <image x='-265' y='-3' width='1023' height='586' href={abc} />
-    </g>
-  </svg>
-)
--- components/Vote/Icons/S.js
+++
@@ -1,9 +0,0 @@
-import abc from './abc'
-
-export default () => (
-  <svg width='55' height='70' viewBox='0 0 55 70'>
-    <g transform='translate(-6 -2)'>
-      <image x='-400' y='-229' width='1023' height='586' href={abc} />
-    </g>
-  </svg>
-)
