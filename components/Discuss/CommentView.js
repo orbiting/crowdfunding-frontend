7ba03e250217b1047361fbe27238faca3c50b9e9
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-05-22 02:06:57 +0200

    improve for mobile

--- components/Discuss/CommentView.js
+++ components/Discuss/CommentView.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {css} from 'glamor'
 
 import {
-  Interaction
+  Interaction, mediaQueries
 } from '@project-r/styleguide'
 
 import pollColors from '../Vote/colors'
@@ -15,12 +15,23 @@ const styles = {
     overflow: 'hidden',
     wordWrap: 'break-word'
   }),
+  title: css({
+    marginBottom: 2,
+    fontSize: 18,
+    [mediaQueries.mUp]: {
+      fontSize: 22
+    }
+  }),
   img: css({
     display: 'block',
     float: 'left',
-    maxWidth: 113,
+    maxWidth: 72,
+    marginBottom: 5,
+    [mediaQueries.mUp]: {
+      marginBottom: 8,
+      maxWidth: 113
+    },
     marginTop: 5,
-    marginBottom: 8,
     marginRight: 15
   })
 }
@@ -30,7 +41,7 @@ export default ({t, feedName, authorImage, authorName, content, tags = []}) => (
     {!!authorImage && (
       <img src={authorImage} {...styles.img} />
     )}
-    <H3 style={{color: pollColors[tags[0]], marginBottom: 2}}>
+    <H3 style={{color: pollColors[tags[0]]}} {...styles.title}>
       {authorName}
       {!!tags[0] && (
         ' ' + t('discuss/comment/for', {
