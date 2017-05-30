fa12fe34ec799f1e202f74ba246339e1b22edb85
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-05-31 00:49:16 +0200

    new index

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,35 @@
 import React, {Component} from 'react'
 import {compose} from 'redux'
 import {timeMinute} from 'd3-time'
+import Link from 'next/link'
 
 import withData from '../lib/withData'
 import withT from '../lib/withT'
 
+import Frame from '../components/Frame'
+import Content from '../components/Frame/Content'
+import VideoCover from '../components/VideoCover'
 import {withStatus} from '../components/Status'
 
-// import {
-//   PUBLIC_BASE_URL, STATIC_BASE_URL
-// } from '../constants'
+import {countFormat} from '../lib/utils/formats'
 
-// import {
-//   Label, Button, Lead, colors,
-//   P, A, linkRule, Interaction
-// } from '@project-r/styleguide'
+import {
+  STATIC_BASE_URL
+} from '../constants'
+
+import {
+  Quote,
+  P, A, linkRule
+} from '@project-r/styleguide'
 
 import {Page as CrowdfundingPage} from './crowdfunding'
 
-// const MERCI_VIDEO = {
-//   hls: 'https://player.vimeo.com/external/213080233.m3u8?s=40bdb9917fa47b39119a9fe34b9d0fb13a10a92e',
-//   mp4: 'https://player.vimeo.com/external/213080233.hd.mp4?s=ab84df0ac9134c86bb68bd9ea7ac6b9df0c35774&profile_id=119',
-//   subtitles: '/static/subtitles/main.vtt',
-//   poster: `${STATIC_BASE_URL}/static/video/main.jpg`
-// }
+const MERCI_VIDEO = {
+  hls: 'https://player.vimeo.com/external/213080233.m3u8?s=40bdb9917fa47b39119a9fe34b9d0fb13a10a92e',
+  mp4: 'https://player.vimeo.com/external/213080233.hd.mp4?s=ab84df0ac9134c86bb68bd9ea7ac6b9df0c35774&profile_id=119',
+  subtitles: '/static/subtitles/main.vtt',
+  poster: `${STATIC_BASE_URL}/static/video/main.jpg`
+}
 
 class Index extends Component {
   constructor (...args) {
@@ -82,10 +88,51 @@ class Index extends Component {
     this.checkTime()
   }
   render () {
+    const {url, crowdfunding} = this.props
     const {ended} = this.state
     if (ended) {
       return (
-        <div>Merci</div>
+        <Frame url={url} meta={{
+          pageTitle: 'Republik — das digitale Magazin von Project R',
+          title: 'Republik — das digitale Magazin von Project R',
+          description: 'Vielen Danke.',
+          image: `${STATIC_BASE_URL}/static/social-media/merci.jpg`
+        }} cover={(
+          <VideoCover src={MERCI_VIDEO} />
+        )}>
+          <div>
+            <Content indented>
+              <P>Ladies and Gentlemen,</P>
+              <P>Thomas Jefferson, der Autor der amerikanischen Unabhängigkeitserklärung, schrieb einmal:</P>
+              <Quote>Ich bin sicher, eine kleine Rebellion dann und wann ist eine gute Sache; sie ist in der Politik so notwendig, um die Dinge zu klären, wie ein Sturm für das Wetter.</Quote>
+
+              <P>Wie es aussieht, war es auch im Journalismus Zeit für eine kleine Rebellion. Sie, unsere {countFormat(crowdfunding.status.people)} Verlegerinnen und Verleger, haben sie unterstützt.</P>
+
+              <P>Wir danken Ihnen für Ihre Neugier, Ihre Entschlusskraft, Ihr Vertrauen. Und für den Mut, in ein neues Modell für Journalismus zu investieren.</P>
+
+              <P>Wir arbeiten nun daran, die Republik zu bauen. Einerseits ein sturmfestes Unternehmen. Und andererseits ein Magazin, bei dem Sie stolz sein können, als Gründerin oder Gründer dabei gewesen zu sein.</P>
+
+              <P>Im Januar 2018 geht es los.</P>
+
+              <P>Mit Dank und Hochachtung,</P>
+
+              <P>Ihre Crew von der Republik und von Project&nbsp;R</P>
+
+              <P>PS: Für die ersten Umbauarbeiten schliessen wir die Anmeldung für neue Mitglieder bis zum 1. Juli. Falls Sie ebenfalls an Bord kommen wollen, tragen Sie hier Ihre Mail-Adresse ein:</P>
+
+              <P>PPS: Auf dem Laufenden über den weiteren Aufbau des digitalen Magazins Republik von Project R bleiben Sie hier: <A href='https://project-r.construction/'>project-r.construction</A></P>
+
+              <P>
+                PPPS:
+                {' '}
+                <Link href='/crowdfunding'>
+                  <a {...linkRule}>Das Crowdfunding-Archiv</a>
+                </Link>
+              </P>
+
+            </Content>
+          </div>
+        </Frame>
       )
     }
 
