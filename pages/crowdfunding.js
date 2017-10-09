6a2aedbcc13095f615d5e9b3b7435ddb7c9a2bac
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-10-09 18:01:57 +0200

    updated cf main video

--- pages/crowdfunding.js
+++ pages/crowdfunding.js
@@ -45,7 +45,7 @@ const styles = {
 const VIDEOS = {
   main: {
     hls: 'https://player.vimeo.com/external/213080233.m3u8?s=40bdb9917fa47b39119a9fe34b9d0fb13a10a92e',
-    mp4: 'https://player.vimeo.com/external/213080233.hd.mp4?s=ab84df0ac9134c86bb68bd9ea7ac6b9df0c35774&profile_id=119',
+    mp4: 'https://player.vimeo.com/external/213080233.hd.mp4?s=ab84df0ac9134c86bb68bd9ea7ac6b9df0c35774&profile_id=175',
     subtitles: '/static/subtitles/main.vtt',
     poster: `${STATIC_BASE_URL}/static/video/main.jpg`
   },
